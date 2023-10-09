abe7d310d5e0db5e3e4851eeaf48eae0b2dd8d47
Viateur <akimanaviateur94@gmail.com>
2023-10-09 13:29:35 -0400

    Exercise3

--- HelloWorld/src/com/viateur/Exercise3.java
+++ HelloWorld/src/com/viateur/Exercise3.java
@@ -1,5 +1,31 @@
 package com.viateur;
-
+import java.util.Scanner;
 public class Exercise3 {
+        // Function to calculate factorial
+        public static int factorial(int num) {
+            if (num == 0 || num == 1) {
+                return 1;
+            } else {
+                return num * factorial(num - 1);
+            }
+        }
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a number to calculate its factorial: ");
+            try {
+                int number = scanner.nextInt();
+                if (number < 0) {
+                    System.out.println("Factorial is not defined for negative numbers.");
+                } else {
+                    int result = factorial(number);
+                    System.out.println("Factorial of " + number + " is " + result);
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            } finally {
+                scanner.close();
+            }
+        }
+    }
 
-}
+
