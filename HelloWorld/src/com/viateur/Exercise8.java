b448a43cac15ab44ac644a0f454a9c6aba4c6e08
Viateur <akimanaviateur94@gmail.com>
2023-10-09 14:12:01 -0400

    Exercise7

---
+++ HelloWorld/src/com/viateur/Exercise8.java
@@ -0,0 +1,13 @@
+package com.viateur;
+
+public class Exercise8 {
+        public static void main(String[] args) {
+            int sum = 0;
+            for (int i = 2; i < 100; i += 2) {
+                sum += i;
+            }
+            System.out.println("Sum of even numbers less than 100: " + sum);
+        }
+    }
+
+
