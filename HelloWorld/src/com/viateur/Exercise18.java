38ed2d1485e82bd2199159b056b081ec324091fe
Viateur <akimanaviateur94@gmail.com>
2023-10-13 13:20:31 -0400

    exercises

---
+++ HelloWorld/src/com/viateur/Exercise18.java
@@ -0,0 +1,22 @@
+package com.viateur;
+
+import java.util.Scanner;
+
+public class Exercise18 {
+    public static String reverse(String fname, String lname){
+        String temp = fname;
+        fname = lname;
+        lname= temp;
+        return fname + " " + lname;
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the firstname:");
+        String firstName = scanner.next();
+        System.out.println(("Enter the lastname"));
+        String lastName = scanner.next();
+
+        String reversedName = reverse(firstName, lastName);
+        System.out.println("The name in reverse" + reversedName);
+    }
+}
