b563188c23120eec50cce832f66fb495d753f010
Viateur <akimanaviateur94@gmail.com>
2023-10-14 09:23:08 -0400

    Exercises

--- HelloWorld/src/com/viateur/EXercise22.java
+++ HelloWorld/src/com/viateur/EXercise22.java
@@ -1,4 +1,24 @@
 package com.viateur;
 
+import java.util.Scanner;
+
 public class EXercise22 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number to check if it is palindrome: - ");
+        int number = scanner.nextInt();
+        int temp = number;
+        int sum=0;
+        int remainder;
+        while(number > 0){
+            remainder = number % 10;
+            number = number / 10;
+            sum = (sum * 10) + remainder;
+        }
+        if(temp == sum){
+            System.out.println("The number is palindrome:-");
+        }else {
+        System.out.println("The number is not palindrome");
+    }
+    }
 }
