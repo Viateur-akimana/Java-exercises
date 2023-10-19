987b670b60b7b8b7bcec1cbb4de3156c2580cad1
Viateur <akimanaviateur94@gmail.com>
2023-10-19 21:50:19 +0200

    solutions

--- HelloWorld/src/com/viateur/Exercise32.java
+++ HelloWorld/src/com/viateur/Exercise32.java
@@ -1,31 +1,29 @@
 package com.viateur;
-
 import java.util.Scanner;
 
 public class Exercise32 {
-
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             char choice;
 
             do {
-                // Displaying the calculator menu
+                // Displaying the menu
                 System.out.println("MENU");
                 System.out.println("1. Add");
                 System.out.println("2. Subtract");
                 System.out.println("3. Multiply");
                 System.out.println("4. Divide");
                 System.out.println("5. Modulus");
-
-                // Asking for user input
                 System.out.print("Enter your choice: ");
                 int operationChoice = scanner.nextInt();
+
+                // Getting two numbers from the user
                 System.out.print("Enter your two numbers: ");
                 int num1 = scanner.nextInt();
                 int num2 = scanner.nextInt();
-
-                // Performing the chosen operation
                 int result = 0;
+
+                // Performing the selected operation
                 switch (operationChoice) {
                     case 1:
                         result = num1 + num2;
@@ -37,24 +35,13 @@ public class Exercise32 {
                         result = num1 * num2;
                         break;
                     case 4:
-                        if (num2 != 0) {
-                            result = num1 / num2;
-                        } else {
-                            System.out.println("Error: Division by zero!");
-                            continue;
-                        }
+                        result = num1 / num2;
                         break;
                     case 5:
-                        if (num2 != 0) {
-                            result = num1 % num2;
-                        } else {
-                            System.out.println("Error: Modulus by zero!");
-                            continue;
-                        }
+                        result = num1 % num2;
                         break;
                     default:
                         System.out.println("Invalid choice!");
-                        continue;
                 }
 
                 // Displaying the result
